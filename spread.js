981d11ea2cfab5916e415196d13a85a96c476e52
Roshan Joshi <i156977@d2hs.com>
2020-05-02 16:13:41 +0545

    update

--- spread.js
+++ spread.js
@@ -1,3 +1,5 @@
+//simple example
+
 var a = 30;
 var b = 20;
 var c = 10;
@@ -7,3 +9,26 @@ function display(...args){
 }
 
 display(a, b, c);
+
+
+
+//-------------slightly complex------------------//
+
+var adder = (...variables)=>{
+    var result = 0;
+    for(element of variables){
+        result += element;
+    }
+    console.log(result);
+}
+
+var multiplier = (...variables)=>{
+    var result = 1;
+    for(element of variables){
+        result *= element;
+    }
+    console.log(result);
+}
+
+adder(1,2,3,4);
+multiplier(2,2,2,2);
