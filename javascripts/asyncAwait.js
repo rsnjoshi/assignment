6cc48db3d5c74a5a3ca45ffd1b164df07603c798
Roshan Joshi <i156977@d2hs.com>
2020-05-04 09:35:19 +0545

    final

--- javascripts/asyncAwait.js
+++ javascripts/asyncAwait.js
@@ -26,9 +26,6 @@ example()
     })
 
 
-
-
-
 //-----------------little complex example-------------------------//
 
 var fetch = require('node-fetch')
@@ -42,14 +39,21 @@ async function extractor(username){
     return githubUser;
 }
 
-extractor('facebook')
+extractor('ashok')
     .then((response)=>{
+        console.log(response);
         console.log(" ");
         console.log("Dear", response.name);
         console.log(" ");
-        console.log("You are from ", response.location, ". You have total", response.followers, "followers and you are following", response.following, ". Your account was created at", response.created_at);
+        console.log("You are from ", response.location, ". You have total", response.followers, "followers and you are following", response.following);
+        console.log(". Your account was created at", response.created_at);
+        console.log(" ");
+        console.log("you work in ", response.company);
         console.log(" ");
         console.log("your blog: ", response.blog);
+        console.log(" ");
+        console.log("your bio: ", response.bio);
+        console.log("your id: ", response.id);
         console.log(" ");
         console.log("Thank you");
         console.log("Laptop");
